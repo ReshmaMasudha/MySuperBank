7c095c91162ee7c0f46fe7f7c53271d6b09cdc8a
reshma <reshmarising1999@gmail.com>
2020-07-22 17:40:25 +0530

    nineth commit

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank
+{
+    class BankAccount
+    {
+        public string number { get; }
+
+        public string owner { get; set; }
+
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        private int accountSeed { get; set; } = 123456;
+        public BankAccount(string name,decimal initialBalance)
+        {
+            this.owner = name;
+            this.number = accountSeed.ToString();
+            accountSeed++;
+            MakeDeposit(initialBalance, DateTime.Now, "initial balance");
+           
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("insufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string accounthistory()
+        {
+            var report = new StringBuilder();
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($"{item.Date }\t{item.Amount}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Reshma",100000000);
+            Console.WriteLine($"a new account {account.number} is opened for {account.owner} with a balance of {account.Balance}");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Me");
+            Console.WriteLine(account.Balance);
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.accounthistory());
+           
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
